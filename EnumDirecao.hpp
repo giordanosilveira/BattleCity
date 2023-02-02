57eefc1f0c153794039d7f8e90fdc7255d19b3bd
Giba pingo <giordanovraau0204@gmail.com>
2023-02-02 17:20:06 -0300

    Arrumando o nome da classe

--- EnumDirecao.hpp
+++ EnumDirecao.hpp
@@ -1,7 +1,7 @@
 #ifndef ENUM_DIRECAO_HPP
 #define ENUM_DIRECAO_HPP
 
-enum class Direcao : unsigned short int {
+enum class EnumDirecao : unsigned short int {
     DIREITA = 48,
     ESQUERDA,
     CIMA,
