fd2be02e6fbb75f3412ea336ec3ae2d543e47f86
Giba pingo <giordanovraau0204@gmail.com>
2023-02-02 17:18:44 -0300

    adicionando o atirar, friendship com a classe Tiro

--- Tanque.cpp
+++ Tanque.cpp
@@ -1,2 +1,49 @@
-#include "Tiro.hpp"
+#include "Tanque.hpp"
+#include "EnumDirecao.hpp"
+#include "EnumEstadoObjeto.hpp"
 
+/**
+ * @todo: Colocar a velocidade como uma constante 
+ * 
+ */
+void Tanque::atirar() {
+
+    unsigned int y;
+    unsigned int x;
+
+    switch (this->direcao)
+    {
+        case (unsigned char)EnumDirecao::DIREITA:
+            y = (this->coordenada->getY() + this->getTamanhoSprite())/2;
+            x = this->coordenada->getX() + this->getTamanhoSprite();
+        break;
+        case (unsigned char)EnumDirecao::ESQUERDA:
+            y = (this->coordenada->getY() + this->getTamanhoSprite())/2;
+            x = this->coordenada->getX();
+        break;
+        case (unsigned char)EnumDirecao::CIMA:
+            y = this->coordenada->getY();
+            x = (this->coordenada->getX() + this->getTamanhoSprite())/2;
+        break;
+        case (unsigned char)EnumDirecao::BAIXO:
+            y = (this->coordenada->getY() + this->getTamanhoSprite());
+            x = (this->coordenada->getX() + this->getTamanhoSprite())/2;
+        break;
+    }
+
+    unsigned short int tamanhoSprite = 4;
+    bool imortal = false;
+    unsigned char estado = (unsigned char)EnumEstadoObjeto::VIVO;
+
+    try {
+        Tiro *tiro{new Tiro{x, y, tamanhoSprite, imortal, estado, 2, (unsigned char)EnumDirecao::DIREITA}};
+        tiro->setTanque(this);
+    }
+    catch (std::bad_alloc& ba) {
+        std::cerr << "Erro na alocação do tiro" << std::endl;
+    }
+    catch (std::runtime_error& re) {
+        std::cerr << "Erro na alocação do tiro" << std::endl;
+    }
+
+}
--- Tanque.hpp
+++ Tanque.hpp
@@ -6,11 +6,18 @@
 #include <iostream>
 #include <list>
 
+
 class Tanque : public Objeto{
+
+    friend void Tiro::setTanque(Tanque*tanque);
+
     public:
-        Tanque(/* args */);
-        ~Tanque();
-    
+        Tanque();
+        Tanque(const unsigned int x, const unsigned int y, unsigned short int tamanhoSprite, const unsigned short imortal, const unsigned short estado, const unsigned short velocidade, const unsigned short direcao);
+        virtual ~Tanque();
+
+        void atirar();
+
     private:
         std::list<Tiro*>tiros;
         unsigned char velocidade;
