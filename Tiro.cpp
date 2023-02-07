5c87fb0ecd939098cc60314ced46fcd042077c94
Giba pingo <giordanovraau0204@gmail.com>
2023-02-07 14:36:21 -0300

    colisão do tiro. As colisões são diferentes

--- Tiro.cpp
+++ Tiro.cpp
@@ -17,3 +17,50 @@ void Tiro::setTanque (Tanque *tanque) {
     this->tanque = tanque;
     if (this->tanque != nullptr);
 }
+
+const bool Tiro::colisao(std::list<Objeto*>&objetos) const {
+
+    unsigned short x_tiro = this->coordenada->getX();
+    unsigned short y_tiro = this->coordenada->getY();
+    unsigned short tam_tiro = (unsigned short)this->getTamanhoSprite();
+    
+    std::list<Objeto*>::const_iterator it;
+
+    it = objetos.begin();
+    for (it; it != objetos.end(); ++it) {
+        
+        unsigned short int x_obj = this->coordenada->getX();
+        unsigned short int y_obj = this->coordenada->getY();
+        unsigned short int tam_obj = (unsigned short) (*it)->getTamanhoSprite();
+
+        switch (this->direcao)
+        {
+            case (unsigned char)EnumDirecao::DIREITA:
+                if ((y_tiro >= y_obj && y_tiro <= y_obj + tam_obj)) {
+                    if (x_tiro + tam_tiro >= x_obj)
+                        return true;
+                }
+                break;
+            case (unsigned char)EnumDirecao::ESQUERDA:
+                if ((y_tiro >= y_obj && y_tiro <= y_obj + tam_obj)) {
+                    if (x_tiro <= x_obj + tam_obj)
+                        return true;
+                }
+                break;
+            case (unsigned char)EnumDirecao::BAIXO:
+                if ((x_tiro >= x_obj && x_tiro <= x_obj + tam_obj)) {
+                    if (y_tiro + tam_tiro >= x_obj)
+                        return true;
+                }
+                break;
+            case (unsigned char)EnumDirecao::CIMA:
+                if ((x_tiro >= x_obj && x_tiro <= x_obj + tam_obj)) {
+                    if (y_tiro <= y_obj + tam_obj)
+                        return true;
+                }
+                break;
+        }
+    }
+    return false;
+
+}
