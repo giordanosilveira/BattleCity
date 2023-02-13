7cfa74a058b15bb3cb5d9be05f0d1451d5ef761d
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2023-02-13 17:13:18 -0300

    Insignia leva dano

--- Estado.cpp
+++ Estado.cpp
@@ -39,7 +39,7 @@ void Estado::jogo(){
                     j->adicionarTiro(t);
             }
             j->atualizarEntidades();
-
+            
             redesenhar = true;
         }
         
